b29870b64bc5193be71f284b0f52ae4a31e9cd49
renanjpaula <renanjohannsen@hotmail.com>
2020-12-11 00:26:54 -0300

    Adição da configuração do wify a partir do arquivo de credenciais

--- wify-ap-config/wify-ap-config.ino
+++ wify-ap-config/wify-ap-config.ino
@@ -7,18 +7,20 @@ IPAddress apGateway(192, 168, 1, 1);
 IPAddress apSubnet(255, 255, 0, 0);
 
 const String AP_NETWORK_NAME = "Config";
+
 const int CONFIG_PIN = 16;
 const int RELAY_PIN = 5;
 
 const String NETWORK_CREDENTIALS_FILE = "network-credentials.txt";
 
-struct networkCredentials {
+struct NetworkCredentials {
   String networkName;
   String password;
 };
 
 ESP8266WebServer server(80);
 
+/* SETUP HARDWARE */
 void setupConfigPin() {
   pinMode(CONFIG_PIN, INPUT);
 }
@@ -28,10 +30,17 @@ void setupRelayPin() {
   digitalWrite(RELAY_PIN, LOW);
 }
 
+void setupHardware() {
+  setupConfigPin();
+  setupRelayPin();
+}
+
+/* BUSINESS FUNCTIONS */
 boolean isConfigSetup() {
   return digitalRead(CONFIG_PIN) == 1;
 }
 
+/* NETWORK CONFIG */
 void setupConfigAccessPoint() {
   WiFi.mode(WIFI_AP);
   Serial.println(WiFi.softAPConfig(apIP, apGateway, apSubnet) ? "Network Configured!" : "Config Network Failed!");
@@ -42,6 +51,28 @@ void setupConfigAccessPoint() {
   Serial.println(WiFi.softAPIP());
 }
 
+void setupNetwork() {
+  struct NetworkCredentials credentials = readCredentials();
+
+  Serial.print("Connection to: ");
+  Serial.println(credentials.networkName);
+  Serial.print("With password: ");
+  Serial.println(credentials.password);
+  
+  WiFi.begin(credentials.networkName, credentials.password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Waiting to connect...");
+  }
+
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/* FILE SYSTEM STORAGE */
 void setupFileSystem() {
   if (!SPIFFS.begin()) {
     Serial.println("Error mounting the file system");
@@ -50,20 +81,20 @@ void setupFileSystem() {
   Serial.println("File System Ready!");
 }
 
-void handleRoot() {
-  File file = SPIFFS.open("/config-network.html", "r");
-  server.streamFile(file, "text/html");
-  file.close();
-}
-
 void saveCredentials(String networkName, String password) {
+  Serial.println("Setting a new network credentials");
+  Serial.print("networkName: ");
+  Serial.println(networkName);
+  Serial.print("password: ");
+  Serial.println(password);
+
   File file = SPIFFS.open(NETWORK_CREDENTIALS_FILE, "w");
   file.println(networkName);
   file.println(password);
   file.close();
 }
 
-struct networkCredentials readCredentials() {
+struct NetworkCredentials readCredentials() {
   Serial.println("Reading network credentials");
   File file = SPIFFS.open(NETWORK_CREDENTIALS_FILE, "r");
 
@@ -73,49 +104,45 @@ struct networkCredentials readCredentials() {
   }
 
   int line = 0;
-  struct networkCredentials credentials;
+  struct NetworkCredentials credentials;
 
   while (file.available()) {
     if (line == 0) {
       credentials.networkName = file.readStringUntil('\n');
+      credentials.networkName.trim();
     }
 
     if (line == 1) {
-      credentials.password  = file.readStringUntil('\n');
+      credentials.password = file.readStringUntil('\n');
+      credentials.password.trim();
     }
 
     line++;
   }
 
   file.close();
-
   return credentials;
+}
+
+/* HTTP SERVER */
+void handleConfigPage() {
+  File file = SPIFFS.open("/config-network.html", "r");
+  server.streamFile(file, "text/html");
+  file.close();
 }
 
 void handleSaveConfig() {
   String networkName = server.arg("networkName");
   String password = server.arg("password");
-
-  Serial.println("Setting a new network credentials");
-  Serial.print("networkName: ");
-  Serial.println(networkName);
-  Serial.print("password: ");
-  Serial.println(password);
-
   saveCredentials(networkName, password);
   server.send(200);
 }
 
 void setupConfigHttpServer() {
-  server.on("/", HTTP_GET, handleRoot);
+  server.on("/", HTTP_GET, handleConfigPage);
   server.on("/config", HTTP_POST, handleSaveConfig);
   server.begin();
   Serial.println("Config server listening on port 80");
-}
-
-void setupHardware() {
-  setupConfigPin();
-  setupRelayPin();
 }
 
 void setup() {
@@ -124,18 +151,13 @@ void setup() {
   setupHardware();
   setupFileSystem();
 
-  struct networkCredentials credentials = readCredentials();
-
-  Serial.println(credentials.networkName);
-  Serial.println(credentials.password);
-
   if (isConfigSetup()) {
     Serial.println("Config setup mode on");
     setupConfigAccessPoint();
     setupConfigHttpServer();
   } else {
     Serial.println("Operation mode on");
-
+    setupNetwork();
   }
 }
 
